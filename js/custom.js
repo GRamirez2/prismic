04936e3c99c62d69db81d8a363a0c1a9f215d3c9
George Ramirez <george.ramirez2@gmail.com>
2017-09-27 16:05:25 -0500

    slides and prismico

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+$( document ).ready(function() {
+  var Prismic = PrismicJS;
+  
+  Prismic.getApi("https://test-site2.prismic.io/api/v2").then(function(api) {
+    // console.log("api", api)
+    return api.getByUID('home', 'george');
+  }).then(function(document) {
+    console.log("document", document)
+    console.log('title', document.data.title1[0].text)
+    // $('#image').attr("src", document.data.image_one.url);
+    // $('#title').html(document.data.title1[0].text);
+    // $('#description').html(PrismicDOM.RichText.asHtml(document.data.description));
+    // $('#title').html(PrismicDOM.RichText.asText(document.tags[0]));
+  });
+});
