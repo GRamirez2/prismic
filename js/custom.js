9179a345a565ff96ac9a174f0436e3f84d74bb0b
George Ramirez <george.ramirez2@gmail.com>
2017-09-28 12:13:02 -0500

    connected to new cms repo

--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,17 @@
 $( document ).ready(function() {
   var Prismic = PrismicJS;
   
-  Prismic.getApi("https://test-site2.prismic.io/api/v2").then(function(api) {
-    // console.log("api", api)
-    return api.getByUID('home', 'george');
+  Prismic.getApi("https://liveconnected.prismic.io/api/v2").then(function(api) {
+    return api.getByUID('living-connected', 'liveconnected');
   }).then(function(document) {
     console.log("document", document)
-    console.log('title', document.data.title1[0].text)
+    // console.log('title', document.data.title1[0].text)
+
     // $('#image').attr("src", document.data.image_one.url);
     // $('#title').html(document.data.title1[0].text);
     // $('#description').html(PrismicDOM.RichText.asHtml(document.data.description));
     // $('#title').html(PrismicDOM.RichText.asText(document.tags[0]));
+
   });
 
   // nav bar selected page becomes active
